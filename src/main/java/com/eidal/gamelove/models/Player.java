ce7c92db11346727a2245b411a4d38349532c3b6
root <root@eidal-deb>
2018-10-15 07:11:02 +0200

    First commit

---
+++ src/main/java/com/eidal/gamelove/models/Player.java
@@ -0,0 +1,62 @@
+package com.eidal.gamelove.models;
+
+
+import javax.validation.constraints.NotNull;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name="players")
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    @NotNull
+    String name;
+
+    public Player(){}
+
+    public Player(@NotNull String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(id, player.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
